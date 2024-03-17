7659504ff9d1a9fc11947d9dbaec343cf28799e7
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-03-17 12:45:17 -0400

    small changes on projects sections

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -4,11 +4,13 @@ import './Project.css';
 function Project({ title, description, imageUrl, liveUrl, repoUrl }) {
   return (
     <div className="project">
-      <div className="project-title">{title}</div>
+      <h3 className="project-title">{title}</h3>
       <img src={imageUrl} alt={`Screenshot of ${title}`} />
       <p>{description}</p>
-      <a href={liveUrl} target="_blank" rel="noopener noreferrer">Live Demo</a>
-      <a href={repoUrl} target="_blank" rel="noopener noreferrer">View Code</a>
+      <div className="button-container">
+        <a href={liveUrl} className="live-demo" target="_blank" rel="noopener noreferrer">Live Demo</a>
+        <a href={repoUrl} className="view-code" target="_blank" rel="noopener noreferrer">View Code</a>
+      </div>
     </div>
   );
 }
--- src/sections/Resume/Resume.jsx
+++ src/sections/Resume/Resume.jsx
@@ -6,7 +6,7 @@ function Resume() {
   return (
     <section className="resume">
       <h2>Resume</h2>
-      <p><a href="path-to-your-resume.pdf" target="_blank" rel="noopener noreferrer">Download My Resume</a></p>
+      <p><a href="https://docs.google.com/document/d/1d2CMPY1w4BIr6Rjs-gItBbelLRV9fRLE/edit?usp=sharing&ouid=106644711663638408441&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer">Download My Resume</a></p>
       <h3>Proficiencies</h3>
       <ul>
         <li>HTML & CSS</li>
