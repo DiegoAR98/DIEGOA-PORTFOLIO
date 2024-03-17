cfbe51a775be0684a5e77b7026281fd87e3f3e5d
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-03-17 14:49:32 -0400

    fix image path

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -4,7 +4,7 @@ import './About.css';
 function About() {
   return (
     <section className="about">
-      <img src="src\assets\profile.png" alt="Your Avatar" />
+      <img src="public\profile.png" alt="Your Avatar" />
       <p>
       I've always had a deep interest in technology, ever since I was a youngster. At seven, I began helping my uncle in his LAN gaming center, setting up computers and working on networks. By the age of fifteen, I snagged my first IT job as a basic computer technician. I was really passionate about the field, so I took a bunch of courses to sharpen my skills, from getting to grips with office software to learning about computer repair. Then, at 24, I made my way back into the IT scene in Florida, focusing on fixing laptops. It was around this time that I discovered coding. It seemed pretty challenging at first, but I was determined, and eventually, I grew to love it. One thing I can promise is that I give my absolute best in every project I tackle.
 </p>
