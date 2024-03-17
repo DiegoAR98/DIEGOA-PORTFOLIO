af5207921272658f4835c842e6d9850c4d2dee65
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-03-17 11:14:58 -0400

    Added css for projects

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,16 +1,11 @@
-import React from "react";
-import './Project.css';
-
 function Project({ title, description, imageUrl, liveUrl, repoUrl }) {
   return (
     <div className="project">
-      <img src={imageUrl} alt={`Screenshot of ${title}`}  />
-      <h3>{title}</h3>
+      <div className="project-title">{title}</div>
+      <img src={imageUrl} alt={`Screenshot of ${title}`} />
       <p>{description}</p>
       <a href={liveUrl} target="_blank" rel="noopener noreferrer">Live Demo</a>
       <a href={repoUrl} target="_blank" rel="noopener noreferrer">View Code</a>
     </div>
   );
 }
-
-export default Project;
--- src/sections/Portfolio/Portfolio.jsx
+++ src/sections/Portfolio/Portfolio.jsx
@@ -11,7 +11,28 @@ function Portfolio() {
       liveUrl: "https://student-project-organizer-f32e9a6cf079.herokuapp.com/",
       repoUrl: "https://github.com/DiegoAR98/Student-Project-Organizer",
     },
-    // More projects coming
+    {
+      title: "JavaScript Code Quiz Game",
+      description: "Introducing the JavaScript Code Quiz Game! This interactive app tests your JavaScript knowledge with timed questions, adding a time penalty for incorrect answers. Compete by recording your initials and scores. Ready to play? Let's dive in!",
+      imageUrl: "https://github.com/DiegoAR98/Code-Quiz/blob/main/assets/Screenshot%202023-11-11%20082245.png?raw=true",
+      liveUrl: "https://diegoar98.github.io/Code-Quiz/",
+      repoUrl: "https://github.com/DiegoAR98/Code-Quiz?tab=readme-ov-file",
+    },
+    {
+      title: "Password-Generator",
+      description: "The Password Generator creates strong, secure passwords with customizable options for length and character types. It's user-friendly, accessible across platforms, and prioritizes privacy and security..",
+      imageUrl: "https://github.com/DiegoAR98/Password-Generator/blob/main/assets/03-javascript-homework-demo.png?raw=true",
+      liveUrl: "https://diegoar98.github.io/Password-Generator/",
+      repoUrl: "https://github.com/DiegoAR98/Password-Generator?tab=readme-ov-file",
+    },
+    {
+      title: "National Parks: A Quintet of Natural Wonders",
+      description: "National Parks: A Quintet of Natural Wonders is a web app for exploring and planning trips to the top 5 US national parks. Packed with detailed info and trip tips, it's your go-to for unforgettable outdoor adventures, whether you're a seasoned traveler or a novice explorer..",
+      imageUrl: "https://github.com/DiegoAR98/Project-1/blob/main/assets/project-ss.png?raw=true",
+      liveUrl: "https://diegoar98.github.io/Project-1/",
+      repoUrl: "https://github.com/DiegoAR98/Project-1?tab=readme-ov-file",
+    },
+            
   ];
 
   return (
